bc2e75b5e3296669f2f332fdd47a621f468e0a90
Fareed Sheriff <29508583+meeeeee@users.noreply.github.com>
2022-06-02 16:45:58 -0400

    Create main.cpp

---
+++ convexhull-sweepline/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+struct point{
+	T x, y;
+	point(){}
+	point(T xx, T yy){
+		x = xx;
+		y = yy;
+	}
+	bool vec(point<T> a){return new point<T>(x - a.x, y - a.y);}
+	bool conv(point<T> b, point<T> c){//convexity reverses between lower and upper
+		point<T> vp, v = c.vec(this);
+		vp = new point<T>(x + b.x, y + (c.y-y)*b.x/(c.x-x));
+		return vp.y < b.y;
+	}
+};
+
+template <typename T>
+vector<point<T>> convhull(vector<point<T>> points){// assume no three points colinear
+	point<T> mp = points[0];
+	for(auto p : points) if(mp.x > p.x) mp = p;
+	vector<point<T>> upper, lower, u({mp}), l({mp});
+	for(auto p : points){
+		if(p.y >= mp.y) upper.push_back(p);
+		else lower.push_back(p);
+	}
+	for(auto p : upper){
+		while(u.size() >= 2 && p.conv(u[u.size()-1], u[u.size()-2])) u.pop_back();
+		u.push_back(p);
+	}
+	for(auto p : lower){
+		while(l.size() >= 2 && !p.conv(l[l.size()-1], l[l.size()-2])) l.pop_back();
+		l.push_back(p);
+	}
+	if(u.size()>=2 && l.size()>=2){
+		if(u[u.size()-1].x > l[l.size()-1].x) while(l.size()>=2 && !u[u.size()-1].conv(l[l.size()-1], l[l.size()-2])) l.pop_back();
+		else while(u.size()>=2 && l[l.size()-1].conv(u[u.size()-1], u[u.size()-2])) u.pop_back();
+	}
+	u.insert(u.end(), l.rbegin(), l.rend());
+	u.pop_back();
+	return u;
+}
+
+int main(){}
